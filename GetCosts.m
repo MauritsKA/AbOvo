22f2149282db30b6ade9dc62ede9a89bf6352340
Pieter Schuller <ptschuller@hotmail.com>
2019-02-12 14:12:20 +0100

    Added time matrix

--- GetCosts.m
+++ GetCosts.m
@@ -1,4 +1,4 @@
-function [C] = GetCosts(CostMatrix,CostTravelViaCleaning,Ds,Ws,I,U,O,Wt,Dt,AddressInfo)
+function [C,T] = GetCosts(CostMatrix,CostTravelViaCleaning,Ds,Ws,I,U,O,Wt,Dt,AddressInfo,timeViaCleaning,TimeMatrix)
 
 toSupplierIndex = sum(CostTravelViaCleaning,1) > 0;
 toSupplierLog = repmat(toSupplierIndex,size(CostTravelViaCleaning,1),1);
@@ -24,5 +24,18 @@ C_UUT = C_complete(FromAddressesIndex_U,[ToAddressesIndex_U;ToAddressesIndex]);
 C = [zeros(length(FromAddressesIndex)), C_SUT;...
      zeros(length(FromAddressesIndex_U),length(FromAddressesIndex)), C_UUT;...
      zeros(length(ToAddressesIndex),length([FromAddressesIndex;FromAddressesIndex_U;ToAddressesIndex]))];
+ 
+%% Same for Time
+tempTime = TimeMatrix;
+tempTime(toSupplierLog) = 0;
+T_complete = tempTime + timeViaCleaning;
+
+T_SUT = T_complete(FromAddressesIndex,[ToAddressesIndex_U;ToAddressesIndex]);
+T_UUT = T_complete(FromAddressesIndex_U,[ToAddressesIndex_U;ToAddressesIndex]);
+
+T = [zeros(length(FromAddressesIndex)), T_SUT;...
+     zeros(length(FromAddressesIndex_U),length(FromAddressesIndex)), T_UUT;...
+     zeros(length(ToAddressesIndex),length([FromAddressesIndex;FromAddressesIndex_U;ToAddressesIndex]))];
+
 end
 
