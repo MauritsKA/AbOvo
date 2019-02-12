65897b9dfdecae891825cf7f821289c1d57051cd
Pieter Schuller <ptschuller@hotmail.com>
2019-02-12 12:31:54 +0100

    Return loading times in minutes

---
+++ getLoadingTimes.m
@@ -0,0 +1,7 @@
+function [l_I,l_U,l_O] = getLoadingTimes(I,U,O,s_l_fix,s_l_var)
+% Return loading time in minutes
+l_I = (I.Quantity1/1000) * s_l_var + s_l_fix;
+l_U = (U.Quantity1/1000) * s_l_var + s_l_fix;
+l_O = (O.Quantity1/1000) * s_l_var + s_l_fix;
+
+end
