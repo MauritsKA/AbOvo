d1ba10587cf3fad40821a626b6f42677ffe37ef7
Pieter Schuller <ptschuller@hotmail.com>
2019-02-12 12:02:36 +0100

    Added needed block structure

--- GetCosts.m
+++ GetCosts.m
@@ -1,4 +1,4 @@
-function [C] = GetCosts(CostMatrix,CostTravelViaCleaning)
+function [C] = GetCosts(CostMatrix,CostTravelViaCleaning,Ds,Ws,I,U,O,Wt,Dt,AddressInfo)
 
 toSupplierIndex = sum(CostTravelViaCleaning,1) > 0;
 toSupplierLog = repmat(toSupplierIndex,size(CostTravelViaCleaning,1),1);
@@ -6,7 +6,23 @@ toSupplierLog = repmat(toSupplierIndex,size(CostTravelViaCleaning,1),1);
 tempCosts = CostMatrix;
 tempCosts(toSupplierLog) = 0;
 
-C = tempCosts+CostTravelViaCleaning;
+C_complete = tempCosts+CostTravelViaCleaning;
 
+FromAddressesID = [Ds.HomeAddressID; Ws.FromAddressID; I.ToAddressID];
+FromAddressesID_U = U.ToAddressID;
+ToAddressesID = [O.FromAddressID; Wt.ToAddressID; Dt];
+ToAddressesID_U = U.FromAddressID;
+
+FromAddressesIndex = GetIndex(AddressInfo, FromAddressesID);
+FromAddressesIndex_U = GetIndex(AddressInfo, FromAddressesID_U);
+ToAddressesIndex = GetIndex(AddressInfo, ToAddressesID);
+ToAddressesIndex_U = GetIndex(AddressInfo, ToAddressesID_U);
+
+C_SUT = C_complete(FromAddressesIndex,[ToAddressesIndex_U;ToAddressesIndex]);
+C_UUT = C_complete(FromAddressesIndex_U,[ToAddressesIndex_U;ToAddressesIndex]);
+
+C = [zeros(length(FromAddressesIndex)), C_SUT;...
+     zeros(length(FromAddressesIndex_U),length(FromAddressesIndex)), C_UUT;...
+     zeros(length(ToAddressesIndex),length([FromAddressesIndex;FromAddressesIndex_U;ToAddressesIndex]))];
 end
 
