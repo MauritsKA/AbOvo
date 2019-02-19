ac166408d4eaf153620eac03e07b0e94b00f79d6
Guinio <guiniograuwde@gmail.com>
2019-02-19 10:39:14 +0100

    updated

--- VNS/getTravelTimeBetweenJobs.m
+++ VNS/getTravelTimeBetweenJobs.m
@@ -1,4 +1,4 @@
 function [travelTime] = getTravelTimeBetweenJobs(initialLocationTruck,jobSequence)
-
+load ../NewData/LinkingMatrices
 travelTime = TimeMatrix(sub2ind(size(TimeMatrix),[initialLocationTruck;jobSequence(:,1)],[jobSequence(:,2);initialLocationTruck]));
 
