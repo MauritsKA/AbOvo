3eb035091f91d6435e9b8aef11cc3f37545353c9
Pieter Schuller <ptschuller@hotmail.com>
2019-03-05 13:38:22 +0100

    Unsparsed the output

--- VNS/getSimilarLevel.m
+++ VNS/getSimilarLevel.m
@@ -1,6 +1,6 @@
 function [similarLevel] = getSimilarLevel(p,q)
 %% Similarity own trucks
-similar_owntrucks = sum(sum(p(:,1:257) == q(:,1:257) & p(:,1:257) > 0))
+similar_owntrucks = sum(sum(p(:,1:257) == q(:,1:257) & p(:,1:257) > 0));
 
 %% Similarity charters
 p_jobsWithCharters = sum(p(:,258:end),2);
@@ -9,6 +9,6 @@ q_jobsWithCharters = sum(q(:,258:end),2);
 similar_charters = p_jobsWithCharters' *  q_jobsWithCharters;
 
 %% Total similar lever
-similarLevel = similar_owntrucks + similar_charters;
+similarLevel = full(similar_owntrucks + similar_charters);
 
 end
