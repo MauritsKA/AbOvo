cccee45ef51ab15c8c555f55c51f444b8751eb7e
Pieter Schuller <ptschuller@hotmail.com>
2019-03-05 12:55:03 +0100

    SimiliarLevel (simple edition), old dissimilar deleted

--- VNS/getDisimilarLevel.m
+++
@@ -1,7 +0,0 @@
-function [disimilarLevel] = getDisimilarLevel(p,q)
-% input: 2 solutions of the truck scheduling. Where the input matrices are
-% the same size with the truckIDs on the rows and jobID's on the columns
-% this code computes the dissimalarity level of 2 solutions
-disimilarLevel = sum(sum(p + q == 1));
-
-end
---
+++ VNS/getSimilarLevel.m
@@ -0,0 +1,14 @@
+function [similarLevel] = getSimilarLevel(p,q)
+%% Similarity own trucks
+similar_owntrucks = sum(sum(p(:,1:257) == q(:,1:257) & p(:,1:257) > 0))
+
+%% Similarity charters
+p_jobsWithCharters = sum(p(:,258:end),2);
+q_jobsWithCharters = sum(q(:,258:end),2);
+
+similar_charters = p_jobsWithCharters' *  q_jobsWithCharters;
+
+%% Total similar lever
+similarLevel = similar_owntrucks + similar_charters
+
+end
