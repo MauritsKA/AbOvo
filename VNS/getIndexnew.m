a0734b967480489fb16950da2ceee41f28cf6dc7
Unknown <mau.kort.alt@hotmail.com>
2019-03-08 13:41:20 +0100

    new getIndex

--- VNS/getIndexnew.m
+++ VNS/getIndexnew.m
@@ -1,6 +1,6 @@
-function [Index] = getIndex(IDS)
+function [Index] = getIndexnew(IDS)
 % Get Addressinfo Index for given list of location IDS
-load ../NewData/AddressInfo
+load ../NewData/AddressInfo % load only if not called often
 I = [1:size(AddressInfo,1)]';
 II = repmat(I,1,length(IDS));
 Select = AddressInfo.AddressID == IDS';
